7376622c1aed5c8140f7650c636b954d18936f2f
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-11-06 01:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 //your JS code here. If required.
+
+const tbody = document.querySelector('#output');
+
+const row = `
+		<tr>
+			<td colspan='2'>Loading...</td>
+		</tr>
+`
+tbody.insertAdjacentHTML('afterbegin', row);
+
+function randomTime() {
+      return Math.floor(Math.random() * 3) + 1;
+}
+
+    function createPromise(name) {
+      const time = randomTime();
+      return new Promise((resolve) => {
+        const start = performance.now();
+        setTimeout(() => {
+          const end = performance.now();
+          const seconds = ((end - start) / 1000).toFixed(3);
+          resolve({ name, time: seconds });
+        }, time * 1000);
+      });
+    }
+
+async function runPromises() {
+      const promises = [
+        createPromise('Promise 1'),
+        createPromise('Promise 2'),
+        createPromise('Promise 3')
+      ];
+
+      const startAll = performance.now();
+      const results = await Promise.all(promises);
+      const endAll = performance.now();
+      const totalTime = ((endAll - startAll) / 1000).toFixed(3);
+
+      tbody.innerHTML = '';
+
+      results.forEach((res) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${res.name}</td>
+          <td>${res.time}</td>
+        `;
+        tbody.appendChild(row);
+      });
+
+      const totalRow = document.createElement('tr');
+      totalRow.innerHTML = `
+        <td>Total</td>
+        <td>${totalTime}</td>
+      `;
+      tbody.appendChild(totalRow);
+    }
+
+    // Run all promises
+    runPromises();
+
+
+
